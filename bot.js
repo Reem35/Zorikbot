c057925464b1caad8d6988f98ddfb98e07f8a884
Reem35 <43762012+Reem35@users.noreply.github.com>
2018-10-27 15:26:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,67 +1,77 @@
-à 14:45
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-
-client.on('ready', function() {
-    console.log(`i am ready ${client.user.username}`);
-});
-
-
-client.on('message', message => {
-    
-    let args = message.content.split(' ').slice(1).join(' ');
-    
-  if (message.content === 'ping') {
-      message.channel.send(`<@-{message.author.id}> Ping..!`)
-  }
-  
-  
-  if (message.content.startsWith('الريفكسbc')) {
-          if (!args[0]) {
-message.channel.send("**البريفكسbc <message>**");
-return;
-}
-message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-   m.send(`${args}`);
-
-});
+client.on('message',async msg => {
+  var p = ".";
+  if(msg.content.startsWith(p + "user")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
+ },1000);
+  });
   }
  
 });
-const developers = ["440856489524068352","",""]
-const adminprefix = ".";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**Now Playig   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leaveserver")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wat')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Now Watching   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Now Listening   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'stream')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/akrame35");
-      message.channel.send(`**Now Streaming   ${argresult}**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+client.on('typingStart', (ch, user) => {
+    if(user.presence.status === 'offline') {
+
+        ch.send(${user} تحذير هذا شخص مسوي نفسه اوف لاين ويكتب)
+        .then(msg => {
+            msg.delete(10000)
+        })
+    }
+}) 
+client.on('message', msg => {
+ if(msg.content === ".bot") {
+let embed24 = new Discord.RichEmbed()   
+   .setThumbnail(client.user.avatarURL)
+   .setColor("RANDOM")  
+   .setTitle(`🤖**معلوماتء البوتء**🤖 || ${client.user.tag}`, true)
+   .addField("📜**ZoriK Bot ,#8031*📜", client.user.tag, true)
+   .addField("🤖**Bot Join Servers**🤖", client.guilds.size, true)
+   .addField("👥**Sender**👥", msg.author.tag, true)
+   .addField("🤖:id: *505722690460712960** :id:🤖 ", client.user.id, true)
+   .addField("📆**Bot Created At**📆", `${moment(client.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(client.user.createdAt).fromNow()}\``, true)
+   .addField("🤖**User**🤖", client.users.size, true)
+   .setFooter(`${msg.author.tag}`, `${msg.author.avatarURL}`, true)
+msg.channel.sendEmbed(embed24)
 }
+ });
+client.on('message',message =>{
+    var prefix = ".";
+    if(message.content.startsWith(prefix + 'top')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://media.discordapp.net/attachments/477570106755383307/479229377037598720/22713057_151850495552450_709700562_o.jpg?width=201&height=201")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+
 });
+
 client.login(process.env.BOT_TOKEN);
